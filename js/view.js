949d123e7d5e9f81f29b807e3a1bf7225501e42c
Michael Bilstein <micha3lbilstein@gmail.com>
2016-10-05 16:10:56 -0400

    Added structure for parsing commands

--- js/view.js
+++ js/view.js
@@ -1,7 +1,15 @@
+/*
+ * Adds the command to the history with the directory and username.
+ */
 function enter(input) {
     let line = directoryString + " " + username + "$ " + input;
     addLine(line);
 }
+
+/*
+ * Adds a line to the history with the input.
+ * Does not deal with lines that are too long.
+ */
 
 function addLine(input) {
     let outputParent = document.getElementById("output");
