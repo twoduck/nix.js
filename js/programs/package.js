4a5d77517e4a899c9ffbbfefb620f2021f30aca1
Michael Bilstein <michaelbilstein>
2016-10-05 20:16:14 -0400

    Added package manager

--- js/programs/package.js
+++ js/programs/package.js
@@ -2,16 +2,28 @@ var scripts = document.getElementById("scripts");
 
 function install(package) {
     let url = 'js/programs/' + package + '.js';
-    let response = getPackage(url);
-    let newScript = document.createElement("script");
-    newScript.innerHTML = response;
-    scripts.appendChild(newScript);
+    getPackage(url, function(response) {
+        let newScript = document.createElement("script");
+        newScript.innerHTML = response;
+        scripts.appendChild(newScript);
+        addLine(package + " has been installed.");
+    }, function(errorText) {
+        addLine("There was an error when installing " + package + ".");
+    });
 }
 
-function getPackage(url) {
+function getPackage(url, onSuccess, onError) {
     let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function() {
+      if (xhttp.readyState==4) {
+        if(xhttp.status==200) {
+          alert("Got the response!");
+          onSuccess(xhttp.responseText);
+        } else {
+          onError("Error");
+        }
+      }
+    };
     xhttp.open("GET", url, true);
     xhttp.send();
-    console.log(xhttp);
-    return xhttp.responseText;
 }
