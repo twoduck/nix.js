30341b50ec4d2d3fd6aa95fc38947fca50f51e2c
Meegul304 <micha3lbilstein@gmail.com>
2016-10-11 04:36:40 -0400

    Cleaned up lazy declarations and debugging

--- js/programs/cowsay.js
+++ js/programs/cowsay.js
@@ -1,5 +1,5 @@
 function cowsay(args) {
-    let text = args.join(" ");
+    const text = args.join(" ");
     const maxLength = (38 > text.length) ? text.length : 38;
     let on = 0;
     addLine(" " + "-".repeat(maxLength + 2) + " ");
@@ -26,8 +26,6 @@ function cowsay(args) {
 function rightPad(text, length) {
     if (text.length >= length)
         return text;
-    let spaces = " ".repeat(length - text.length);
-    console.log(text);
-
+    const spaces = " ".repeat(length - text.length);
     return text + spaces;
 }
