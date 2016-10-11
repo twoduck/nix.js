3ca64edfc406903a0c01795e493a65a5b9ad4b67
Meegul304 <micha3lbilstein@gmail.com>
2016-10-11 12:27:59 -0400

    Added ability to uninstall packages

--- js/programs/package.js
+++ js/programs/package.js
@@ -2,44 +2,83 @@ var scripts = document.getElementById("scripts");
 
 function pkg(args) {
     if (args[0] === "install") {
-        if (args.length != 2 && args.length != 3) {
-            addLine("Please include just a package name.");
-            return;
-        }
-        console.log(args);
-        let package = args[1];
-        let shouldPrint = (args[2] === undefined) ? true : args[2] == true; //Print by default
-        if (document.getElementById(package)) {
-            addLine(package + " is already installed.");
-            return;
-        }
-        let url = 'js/programs/' + package + '.js';
-        getPackage(url, function (response) {
-            let newScript = document.createElement("script");
-            newScript.innerHTML = response;
-            newScript.id = package;
-            newScript.type = "text/javascript";
-            scripts.appendChild(newScript);
-            addToPackageList(package);
-            if (shouldPrint)
-                addLine(package + " has been installed.");
-        }, function (errorText) {
-            addLine("There was an error when installing " + package + ".");
-        });
+        install(args[1], args[2]);
     }
     if (args[0] === "list") {
-        let packageList = Cookies.get('packages');
-        if (packageList) {
-            packageList.split(",").sort().forEach(function (element) {
-                addLine(element);
-            }, this);
-        } else {
-            addLine("You do not have any packages installed.");
-        }
+        list();
+    }
+    if (args[0] === "uninstall") {
+        uninstall(args[1], args[2]);
     }
 }
 
-function getPackage(url, onSuccess, onError) {
+let list = function() {
+    let packageList = Cookies.get('packages');
+    if (packageList) {
+        packageList.split(",").sort().forEach(function (element) {
+            addLine(element);
+        }, this);
+    } else {
+        addLine("You do not have any packages installed.");
+    }
+}
+
+let uninstall = function(packageName, shouldPrint) {
+    let packageList = Cookies.get("packages");
+    if (!packageList) {
+        addLine("You don't have any packages to uninstall.");
+        return;
+    }
+    let packagesParent = document.getElementById("scripts");
+    let element = document.getElementById(packageName);
+    if (!element) {
+        addLine(packageName + " is not installed.");
+        return;
+    }
+    packagesParent.removeChild(element);
+    window[packageName] = undefined;
+    let newPackageList = "";
+    let packagesInstalled = packageList.split(",");
+    newPackageList = packagesInstalled.sort().map(function(element) {
+        if (element !== packageName)
+            return element;
+        else return " ";
+    }).reduce(function(total, element) {
+        if (element !== " ")
+            if (total === " ")
+                return element;
+            else return total + "," + element;
+        else return total;
+    });
+    console.log(newPackageList);
+    Cookies.set("packages", newPackageList);
+}
+
+let install = function(packageName, shouldPrint) {
+    if (!packageName) {
+            addLine("Please include a package name.");
+            return;
+        }
+        if (document.getElementById(packageName)) {
+            addLine(packageName + " is already installed.");
+            return;
+        }
+        let url = 'js/programs/' + packageName + '.js';
+        getPackage(url, function (response) {
+            let newScript = document.createElement("script");
+            newScript.innerHTML = response;
+            newScript.id = packageName;
+            newScript.type = "text/javascript";
+            scripts.appendChild(newScript);
+            addToPackageList(packageName);
+            if (shouldPrint === undefined || shouldPrint)
+                addLine(packageName + " has been installed.");
+        }, function (errorText) {
+            addLine("There was an error when installing " + packageName + ".");
+        });
+}
+
+let getPackage = function(url, onSuccess, onError) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4) {
@@ -54,7 +93,7 @@ function getPackage(url, onSuccess, onError) {
     xhttp.send();
 }
 
-function addToPackageList(name) {
+let addToPackageList = function(name) {
     let packageList = Cookies.get('packages');
     if (!packageList) {
         packageList = name;
