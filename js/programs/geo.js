1c740ead9e3daf15d479a881787379032b38b13f
Meegul304 <micha3lbilstein@gmail.com>
2016-10-12 13:24:22 -0400

    Linting

--- js/programs/geo.js
+++ js/programs/geo.js
@@ -2,17 +2,17 @@ let line;
 
 function geo(args) {
     if (args.length !== 0) {
-        addLine('This command does not accept inputs.');
+        addLine("This command does not accept inputs.");
     }
     
-    if (localStorage.getItem('geoPermission') === null) {
-        addLine('Please accept this request if you would like to see your location.');
-    } else if (localStorage.getItem('geoPermission') === 0) {
-        addLine('You have previously declined to grant permission to locate you.');
+    if (localStorage.getItem("geoPermission") === null) {
+        addLine("Please accept this request if you would like to see your location.");
+    } else if (localStorage.getItem("geoPermission") === 0) {
+        addLine("You have previously declined to grant permission to locate you.");
         return;
     }
     
-    line = addLine('This will be replaced shortly...');
+    line = addLine("This will be replaced shortly...");
     
     // Check to see if the browser supports the GeoLocation API.
     if (navigator.geolocation) {
@@ -22,23 +22,23 @@ function geo(args) {
             maximumAge: 0
         };
                 
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition((position) => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
             changeLine(line, `Your location: ${lat.toFixed(5)}, ${lon.toFixed(5)}`);
-            localStorage.setItem('geoPermission', 1);
+            localStorage.setItem("geoPermission", 1);
         }, error, options);
     } else {
-        changeLine(line, 'Your browser does not support geolocation.');
+        changeLine(line, "Your browser does not support geolocation.");
     }
 }
 
 const error = function(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
     if (err.code === 1) {
-        localStorage.setItem('geoPermission', 0);
-        changeLine(line, 'You have declined to grant permission to locate you.');
+        localStorage.setItem("geoPermission", 0);
+        changeLine(line, "You have declined to grant permission to locate you.");
     } else {
-        changeLine(line, 'An unexpected error has occured. Please try again.');
+        changeLine(line, "An unexpected error has occured. Please try again.");
     }
 };
