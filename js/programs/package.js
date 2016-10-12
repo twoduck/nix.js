c6e7612cc10801dfae701e1627dcada9346f06ef
Meegul304 <micha3lbilstein@gmail.com>
2016-10-11 20:32:46 -0400

    Linting

--- js/programs/package.js
+++ js/programs/package.js
@@ -23,82 +23,82 @@ function pkg(args) {
     }
 }
 
-let list = function() {
-    let packageList = localStorage.getItem("packages");
+const list = function() {
+    const packageList = localStorage.getItem("packages");
     if (packageList) {
-        packageList.split(",").sort().forEach(function (element) {
+        packageList.split(",").sort().forEach((element) => {
             addLine(element);
         }, this);
     } else {
         addLine("You do not have any packages installed.");
     }
-}
+};
 
-let uninstall = function(packageName, shouldPrint) {
-    let packageList = localStorage.getItem("packages");
+const uninstall = function(packageName, shouldPrint) {
+    const packageList = localStorage.getItem("packages");
     if (!packageList) {
         addLine("You don't have any packages to uninstall.");
         return;
     }
-    let packagesParent = document.getElementById("scripts");
-    let element = document.getElementById(packageName);
+    const packagesParent = document.getElementById("scripts");
+    const element = document.getElementById(packageName);
     if (!element) {
-        addLine(packageName + " is not installed.");
+        addLine(`${packageName} is not installed.`);
         return;
     }
     packagesParent.removeChild(element);
     window[packageName] = undefined;
     let newPackageList = "";
-    let packagesInstalled = packageList.split(",");
-    newPackageList = packagesInstalled.sort().map(function(element) {
+    const packagesInstalled = packageList.split(",");
+    newPackageList = packagesInstalled.sort().map((element) => {
         if (element !== packageName)
             return element;
         else return " ";
-    }).reduce(function(total, element) {
+    }).reduce((total, element) => {
         if (element !== " ")
             if (total === " ")
                 return element;
-            else return total + "," + element;
+            else return `${total},${element}`;
         else return total;
     });
     localStorage.setItem("packages", newPackageList);
     if (shouldPrint || shouldPrint === undefined)
-        addLine(packageName + " has been uninstalled.");
-}
+        addLine(`${packageName} has been uninstalled.`);
+};
 
-let install = function(packageName, shouldPrint) {
+const install = function(packageName, shouldPrint) {
     if (!packageName) {
-            addLine("Please include a package name.");
-            return;
-        }
-        if (document.getElementById(packageName)) {
-            addLine(packageName + " is already installed.");
-            return;
-        }
-        let url = 'js/programs/' + packageName + '.js';
-        getPackage(url, function (response) {
-            let newScript = document.createElement("script");
-            newScript.innerHTML = response;
-            newScript.id = packageName;
-            newScript.type = "text/javascript";
-            scripts.appendChild(newScript);
-            addToPackageList(packageName);
-            if (shouldPrint === undefined || shouldPrint)
-                addLine(packageName + " has been installed.");
-        }, function (errorText) {
-            addLine("There was an error when installing " + packageName + ".");
-        });
-}
+        addLine("Please include a package name.");
+        return;
+    }
+    if (document.getElementById(packageName)) {
+        addLine(`${packageName} is already installed.`);
+        return;
+    }
+    const url = `js/programs/${packageName}.js`;
+    getPackage(url, (response) => {
+        const newScript = document.createElement("script");
+        newScript.innerHTML = response;
+        newScript.id = packageName;
+        newScript.type = "text/javascript";
+        scripts.appendChild(newScript);
+        addToPackageList(packageName);
+        if (shouldPrint === undefined || shouldPrint)
+            addLine(`${packageName} has been installed.`);
+    }, (errorText) => {
+        addLine(`There was an error when installing ${packageName}.`);
+    });
+};
 
-let available = function() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+const available = function() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4) {
             if (xhttp.status == 200) {
-                let packages = xhttp.responseText.split(",");
-                packages.forEach(function(element) {
+                const packages = xhttp.responseText.split(",");
+                packages.forEach((element) => {
                     if (document.getElementById(element))
-                        addLine("* " + element);
+                        addLine(`* ${element}`);
                     else addLine(element);
                 }, this);
             } else {
@@ -106,14 +106,14 @@ let available = function() {
             }
         }
     };
-    let url = "js/programs/packages";
+    const url = "js/programs/packages";
     xhttp.open("GET", url, true);
     xhttp.send();
-}
+};
 
-let getPackage = function(url, onSuccess, onError) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+const getPackage = function(url, onSuccess, onError) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4) {
             if (xhttp.status == 200) {
                 onSuccess(xhttp.responseText);
@@ -124,14 +124,14 @@ let getPackage = function(url, onSuccess, onError) {
     };
     xhttp.open("GET", url, true);
     xhttp.send();
-}
+};
 
-let addToPackageList = function(name) {
+const addToPackageList = function(name) {
     let packageList = localStorage.getItem('packages');
     if (!packageList) {
         packageList = name;
     } else if (packageList.split(",").indexOf(name) == -1) { //only add to package list if it's not already in it
-        packageList += ',' + name;
+        packageList += `,${name}`;
     }
     localStorage.setItem("packages", packageList);
-}
+};
