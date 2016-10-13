b45ad0200d4ac1016e05693f4569c0b490569960
Meegul304 <micha3lbilstein@gmail.com>
2016-10-13 12:21:59 -0400

    no longer allows you to navigate into files

--- js/programs/cd.js
+++ js/programs/cd.js
@@ -3,8 +3,16 @@ function cd(args) {
         return;
     }
     const lookedUp = resolveResource(args[0]);
+    if (typeof lookedUp.content === "string") {
+        stderr("Not a directory.");
+        addLine(`${args[0]} is not a directory`);
+        return;
+    }
     if (lookedUp) {
         directoryIn = lookedUp;
         updateDirectoryString();
+    } else {
+        stderr("Directory not found.");
+        addLine("Directory not found.");
     }
 }
--- js/programs/loadingBar.js
+++ js/programs/loadingBar.js
@@ -1,16 +1,4 @@
-var percentage;
-var intervalID;
-
-function loadingBar(index) {
-    percentage = 0;
-    intervalID = window.setInterval(updateBar, 50, index, intervalID);
-}
-
-function updateBar(index, ID) {
-    let line = percentage + "% " + "|".repeat(percentage/2);
+function loadingBar(index, percentage) {
+    const line = `${percentage}% ${"|".repeat(Number(percentage)/2)}`;
     changeLine(index, line);
-    percentage++;
-    if (percentage > 100) {
-        clearInterval(intervalID);
-    }
 }
