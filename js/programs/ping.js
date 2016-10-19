ed4e18aa249dd638acdd94b1221d6587ec99d01d
Meegul304 <micha3lbilstein@gmail.com>
2016-10-18 20:07:35 -0400

    Added ping package

---
+++ js/programs/ping.js
@@ -0,0 +1,38 @@
+(function() {
+    function ping(host, port, pong) {
+
+        var started = new Date().getTime();
+
+        var http = new XMLHttpRequest();
+
+        http.open("GET", "http://" + host + ":" + port, /*async*/true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+            var ended = new Date().getTime();
+
+            var milliseconds = ended - started;
+
+            if (pong != null) {
+                pong(milliseconds);
+            }
+            }
+        };
+        try {
+            http.send(null);
+        } catch(exception) {
+            // this is expected
+        }
+
+    }
+
+    if (!stdin()) {
+        stderr("No domain given.");
+        return;
+    }
+    const domain = stdin();
+
+    ping(domain, 80, function(time) {
+        addLine(`It took ${time} milliseconds to reach ${domain}.`);
+    });
+
+}())
